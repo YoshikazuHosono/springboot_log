e78e9fea905ac07ccb644e32c78a9e26af492e30
YoshikazuHosono <yyhhwork@gmail.com>
2020-06-28 10:53:36 +0900

    [add] simple logback setting

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class DemoController {
 
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
     @GetMapping("/hello/{name}")
     public String hello(@PathVariable String name) {
+        logger.info(name + " info!");
+        logger.debug(name + " debug!");
+        logger.error(name + " error!");
+        logger.trace(name + " trace!");
+        logger.warn(name + " warn!");
         return "hello " + name + "!";
     }
 
